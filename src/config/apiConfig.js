0c9cd57f15362b0914f6ed26d177bddeb3aa221e
Ivan <pijak.ivan@gmail.com>
2026-02-18 15:34:56 +0100

    Fixed AWS bugs

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,6 +6,10 @@ export const IS_LOCAL = hostname === 'localhost' || hostname === '127.0.0.1';
 export const IS_AZURE = hostname.includes('azurestaticapps.net');
 export const IS_AWS = hostname.includes('amazonaws.com'); // Spremno za kasnije
 
+console.log('HOSTNAME:', hostname);
+console.log('IS_AWS:', IS_AWS);
+console.log('IS_AZURE:', IS_AZURE);
+
 // ######
 // 2. ŠTO KORISTIM ZA AUTH/DATA? (Firebase ili Backend)
 // ######
@@ -19,12 +23,14 @@ export const USE_FIREBASE = false;
 const URLS = {
     LOCAL: 'http://localhost:8000/api',
     AZURE: 'https://whiteboard-backend-dpg3g0asfrhqgxg5.westeurope-01.azurewebsites.net/api',
-    AWS: 'http://whiteboard-production.eba-uti7h2qf.eu-north-1.elasticbeanstalk.com/'
+    AWS: 'http://whiteboard-production.eba-uti7h2qf.eu-north-1.elasticbeanstalk.com/api'
 };
 
 // Određivanje baze URL-a ovisno o tome gdje je aplikacija pokrenuta
 let selectedUrl = URLS.LOCAL; // Default
 if (IS_AZURE) selectedUrl = URLS.AZURE;
 if (IS_AWS) selectedUrl = URLS.AWS;
+
+console.log('SELECTED URL:', selectedUrl);
 
 export const API_BASE_URL = selectedUrl;
